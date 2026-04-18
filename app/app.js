70a4fa095e689ec3184a560cfb5c96adfdbfbdca
Vinay <vinaysg205@gmail.com>
2026-04-18 23:40:49 +0530

    added login page

--- app/app.js
+++ app/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 👇 IMPORTANT: serve login.html
 app.get('/', (req, res) => {
-  res.send('Hello from EKS DevOps Project 🚀🚀');
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === 'admin@test.com' && password === '1234') {
+    res.send('<h2>Login Successful 🎉</h2>');
+  } else {
+    res.send('<h2>Invalid Credentials ❌</h2>');
+  }
 });
 
 app.listen(3000, () => {
